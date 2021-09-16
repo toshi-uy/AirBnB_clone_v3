fb18c77db5168873c6dfe46fcd8c56407518a227
toshi-uy <s_borgia@hotmail.com>
2021-09-16 16:18:27 -0300

    testing

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -78,7 +78,7 @@ class DBStorage:
     def get(self, cls, id):
         """method to retrieve one object"""
         if cls and id:
-            objects = self.all(classes[cls])
+            objects = self.all(State)
             for obj in objects:
                 if obj.__class__.__name__ == cls and obj.id == id:
                     return obj
