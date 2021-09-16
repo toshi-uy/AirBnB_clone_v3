a6ac260b50cbcdead2afe7dbbaf894b72214e0d2
Eze <ezedksl@gmail.com>
2021-09-16 18:39:07 +0000

    filestorage get

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -68,3 +68,12 @@ class FileStorage:
     def close(self):
         """call reload() method for deserializing the JSON file to objects"""
         self.reload()
+
+    def get(self, cls, id):
+        """method to retrieve one object"""
+        if cls and id:
+            objects = dir(self.all())
+            for obj in objects:
+                if obj.__class__.__name__ == cls and obj.id == id:
+                    return obj
+        return None
