30452bf1cdbe994418ebf5b5df8de504b4976307
toshi-uy <s_borgia@hotmail.com>
2021-09-17 14:14:27 -0300

    added task 4 api missing folder and completed api.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,13 @@ from models import storage
 from api.v1.views import app_views
 
 app = Flask(__name__)
+app.register_blueprint(app_views)
+
+@app.teardown_appcontext
+def close(self):
+    """method to close session"""
+    storage.close()
 
 if __name__ == '__main__':
     app.run(host=getenv("HBNB_API_HOST", "0.0.0.0"),
-            port=getenv("HBNB_API_PORT", "5000"))
+            port=getenv("HBNB_API_PORT", "5000"), threaded=True)
