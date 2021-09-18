1839bbd1f36801f67136cf928f9a6179343aed86
toshi-uy <s_borgia@hotmail.com>
2021-09-18 16:59:13 -0300

    added full path

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,5 +3,5 @@
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-from views.index import *
-from views.states import *
+from api.v1.views.index import *
+from api.v1.views.states import *
