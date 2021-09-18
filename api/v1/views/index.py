72d5edbfb9cf4b4c288623de97452c93f7fe547e
toshi-uy <s_borgia@hotmail.com>
2021-09-18 17:04:51 -0300

    all to cero

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -4,15 +4,9 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-classes = {
-    "Amenity": 0,
-    "BaseModel": 0,
-    "City": 0,
-    "Place": 0,
-    "Review": 0,
-    "State": 0,
-    "User": 0
-    }
+classes = {"Amenity": 0, "BaseModel": 0, "City": 0,
+           "Place": 0, "Review": 0,"State": 0,
+           "User": 0}
 
 
 @app_views.route("/status")
