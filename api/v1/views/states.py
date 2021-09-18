1953306e036c0bd0f97ecfd25cc97d05e133df1e
toshi-uy <s_borgia@hotmail.com>
2021-09-18 17:31:47 -0300

    testing the state view with the state Id

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 """States module"""
-from flask.json import jsonify
 from api.v1.views import app_views
-from flask import Flask, Request
+from flask import jsonify, abort
 from models import storage
+from models.state import State
 
-@app_views.route('/api/v1/states', methods=['GET'])
-def all_states():
+@app_views.route('/states/', methods=['GET'], strict_slashes=False)
+@app_views.route('/states/<state_id>', methods=['GET'])
+def all_states(state_id=None):
     """Returns all state objects."""
     states = []
     for key, value in storage.all('State').items():
-        states.append(value.to_dict())
-    return jsonify(states)
+        states.append(list(value.to_dict()))
+    if not state_id:
+        return jsonify(states)
+    get_state = storage.get(State, state_id)
+    if get_state is None:
+        abort(404)
+    else:
+        jsonify(get_state.to_dict())
